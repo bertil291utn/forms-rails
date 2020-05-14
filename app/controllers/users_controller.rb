3acb27d856e1a187d40593c66afb398b611a0fc0
Meme <meme_es@hotmail.com>
2020-05-14 16:31:53 -0600

    Modify the 'new.html.erb' to use the 'form_for' tag helper
    This commit uses the 'form_for' tag helper to improve the creation of
    forms into the 'new.html.erb' file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(users_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(users_params)
 
     if @user.save
       redirect_to new_user_path
