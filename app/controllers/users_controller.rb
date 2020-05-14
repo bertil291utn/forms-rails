548aa2dc0e761b9407f150abe6b22588f1904756
Meme <meme_es@hotmail.com>
2020-05-14 16:13:12 -0600

    Modify the 'new.html.erb' file
    This commits use the 'form_tag' helper of Rails to work with forms instead
    the normal HTML form format.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(users_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(users_params)
+
     if @user.save
       redirect_to new_user_path
     else
