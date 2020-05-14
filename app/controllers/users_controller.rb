4824f92224adafa0984c0b09e0d0b56160b49fb8
bertil291utn <bertilt4@gmail.com>
2020-05-14 16:42:12 -0500

    Define create action for user create  object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
