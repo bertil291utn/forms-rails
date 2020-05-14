3ebcf93f92d8247400b6f9ff1da0f69e9f916981
bertil291utn <bertilt4@gmail.com>
2020-05-14 16:51:20 -0500

    Create a new private action to use in new user create object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(users_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
